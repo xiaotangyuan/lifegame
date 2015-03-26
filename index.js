6fda8de8a9250e9ba09e5f99ff1f14f635d09fce
xiaoluo <xiaoluo@qq.com>
2015-03-26 09:17:33 +0800

    重构：MakeCells

--- index.js
+++ index.js
@@ -17,10 +17,6 @@ var canvas=document.getElementById('lifecanvas');
 var ctx=canvas.getContext("2d");
 CELL_WIDTH=10
 CELL_HEIGHT=10
-function getNum(n) {
-	// n为随机数的上限。
-	return Math.floor(Math.random() * ( n + 1));
-}
 // 细胞的对象表示--类
 function Cell (zuobiao) {
 	this.oldstate=0;
@@ -29,35 +25,31 @@ function Cell (zuobiao) {
 	this.zuobiao={x:zuobiao.x,y:zuobiao.y}
 	this.width=CELL_WIDTH;
 	this.height=CELL_WIDTH;
-	this.draw=function (ctx) {
-		// 将当前的状态赋值给旧状态，以给后续的细胞判断生命显示状态
-		// var state_pd=this.state;
-		// this.oldstate=this.state;
-		// 判断是否绘制前先观察周围细胞状态，赋值未来下一次的生命状态
-		var num=getCellLiveNum(this.zuobiao);
-		this.futurestate=getFutureState(num);
-		console.log('----this.futurestate:-------',this.futurestate);
-		// 从全局状态表中得到之前的生存状态，判断是否绘制。
-		// 如果状态为0,表示已经死亡，不绘制
-		if (!this.state) {
-			ctx.fillStyle='white';
-			ctx.fillRect(this.zuobiao.x,this.zuobiao.y,this.width,this.height);
-		}else{
-			var color=this.color();
-			ctx.fillStyle=color;
-			ctx.fillRect(this.zuobiao.x,this.zuobiao.y,this.width,this.height);			
-		}
-	};
+	// this.draw=function (ctx) {
+	// 	// 将当前的状态赋值给旧状态，以给后续的细胞判断生命显示状态
+	// 	// var state_pd=this.state;
+	// 	// this.oldstate=this.state;
+	// 	// 判断是否绘制前先观察周围细胞状态，赋值未来下一次的生命状态
+	// 	var num=getCellLiveNum(this.zuobiao);
+	// 	this.futurestate=getFutureState(num);
+	// 	console.log('----this.futurestate:-------',this.futurestate);
+	// 	// 从全局状态表中得到之前的生存状态，判断是否绘制。
+	// 	// 如果状态为0,表示已经死亡，不绘制
+	// 	if (!this.state) {
+	// 		ctx.fillStyle='white';
+	// 		ctx.fillRect(this.zuobiao.x,this.zuobiao.y,this.width,this.height);
+	// 	}else{
+	// 		var color=this.color();
+	// 		ctx.fillStyle=color;
+	// 		ctx.fillRect(this.zuobiao.x,this.zuobiao.y,this.width,this.height);			
+	// 	}
+	// };
 	// 随机颜色，rgb值
-	this.color=function () {
-		var color="rgb(R,G,B)".replace('R',getNum(255))
-							.replace('G',getNum(255))
-							.replace('B',getNum(255));
-		return color
-	}
+	
 
 	// 获取细胞生存环境活的细胞数字
-	this.getCellLiveNum=function (thezuobiao) {
+	this.getCellLiveNum=function () {
+		var thezuobiao=this.zuobiao;
 		var NeighborZuoBiaos=getNeighborsZuoBiao(thezuobiao);
 		var NeighborCells=getNeighborsCells(NeighborZuoBiaos);
 		var cellsStates=[];
@@ -76,46 +68,58 @@ function Cell (zuobiao) {
 	}
 
 	// 获取未来的状态
-	this.getFutureState=function (num) {
+	this.refreshFutureState=function () {
+		var num=this.getCellLiveNum();
 		// 规则
 		// 1  周围有3个细胞，生
 		// 2  周围有2个细胞，保持之前的生存状态不变
 		// 3  其他情况为死亡
 		if (num==3) {
-			return 1;
+			this.futurestate=1;
+			return;
 		};
 		if (num==2) {
-			return this.state;
+			this.futurestate=this.state;
+			return;
 		};
-		return 0;
+		if (num!=2&&num!=3) {
+			this.futurestate=0;
+			return;
+		};
+		
 	}
 
 }
 
-// 获得细胞坐标数组
-function getZuoBiaos () {
-	var zuobiaos=[];
-	for (var i = 0; i < canvas.width/CELL_WIDTH; i++) {
-		x=i*CELL_WIDTH;
-		for (var j = 0; j < canvas.height/CELL_HEIGHT; j++) {
-			y=j*CELL_WIDTH;
-			zuobiaos.push({x:x,y:y});
+
+function MakeCells (canvas) {
+	// 获得细胞坐标数组
+	this.getZuoBiaos=function () {
+		var zuobiaos=[];
+		for (var i = 0; i < canvas.width/CELL_WIDTH; i++) {
+			x=i*CELL_WIDTH;
+			for (var j = 0; j < canvas.height/CELL_HEIGHT; j++) {
+				y=j*CELL_WIDTH;
+				zuobiaos.push({x:x,y:y});
+			};
 		};
-	};
-	return zuobiaos;
+		return zuobiaos;
+	}
+
+	// 获得坐标对象
+	this.getCells=function() {
+		var zuobiaos=this.getZuoBiaos();
+		var cells=[]
+		for (var i = 0; i < zuobiaos.length; i++) {
+			var x=Cell(zuobiaos[i]);
+			cells.push(new Cell(zuobiaos[i]));
+		};
+		return cells
+	}
+	
 }
 
 
-// 获得坐标对象
-function getCells () {
-	var zuobiaos=getZuoBiaos();
-	var cells=[]
-	for (var i = 0; i < zuobiaos.length; i++) {
-		var x=Cell(zuobiaos[i]);
-		cells.push(new Cell(zuobiaos[i]));
-	};
-	return cells
-}
 
 var cells =getCells();
 // for (var i = 0; i < cells.length; i++) {
@@ -194,7 +198,7 @@ function getNeighborsZuoBiao (zuobiao) {
 }
 
 
-// 输入坐标数组，得到细胞列表；
+// 输入坐标数组，得到数组对应的细胞数组；
 function getNeighborsCells(NeighborZuoBiaos) {
 	var cells=[];
 	for (var i = 0; i < NeighborZuoBiaos.length; i++) {
@@ -204,18 +208,10 @@ function getNeighborsCells(NeighborZuoBiaos) {
 	return cells;
 }
 
-// 随机cell的状态
-function random_cellstate(cells) {
-	for (var i = 0; i < cells.length; i++) {
-		var s=getNum(1);
-		cells[i].state=s;
-	};
-	return cells
-}
 
 
 
-
+// ------------------------测试------------------------------------
 function test_getNeighborsCells() {
 	// x: 0, y: 580
 	var res=getNeighborsZuoBiao({x:0,y:580});
@@ -229,4 +225,24 @@ function test_getNeighborsCells() {
 
 // test_getNeighborsCells();
 
-
+// ---------------------------工具-----------------------
+// 0-n的随机数
+function getNum(n) {
+	// n为随机数的上限。
+	return Math.floor(Math.random() * ( n + 1));
+}
+// 随机颜色
+function randomColor() {
+	var color="rgb(R,G,B)".replace('R',getNum(255))
+							.replace('G',getNum(255))
+							.replace('B',getNum(255));
+	return color
+}
+// 随机cell的状态
+function random_cellstate(cells) {
+	for (var i = 0; i < cells.length; i++) {
+		var s=getNum(1);
+		cells[i].state=s;
+	};
+	return cells
+}
