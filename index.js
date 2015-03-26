46bd9961a1947c8d33a332f9830a9faa79279c08
xiaoluo <xiaoluo@qq.com>
2015-03-26 10:27:17 +0800

    画出格子

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var stopbtn=document.getElementById('stop');
 beginbtn.onclick=function  () {
 	// alert('haha');
 	cells=random_cellstate(cells)
-	// drawBorders(cells);
+
 	ac=setInterval('drawState(cells)',500);
 }
 stopbtn.onclick=function () {
@@ -15,8 +15,36 @@ stopbtn.onclick=function () {
 
 var canvas=document.getElementById('lifecanvas');
 var ctx=canvas.getContext("2d");
-CELL_WIDTH=10
-CELL_HEIGHT=10
+var CELL_WIDTH=10
+var CELL_HEIGHT=10
+// 产生细胞
+var cells=new MakeCells(canvas).getCells();
+// 让每个细胞获得自己的邻居细胞
+setNeighbors(cells,canvas);
+
+// 画出格子
+draw_house();
+
+// 方法定义：画出格子
+function draw_house(){
+	ctx.strokeStyle="gray";
+	for (var i = 0; i < cells.length; i++) {
+		var zb=cells[i].zuobiao;
+		ctx.strokeRect(zb.x,zb.y,CELL_WIDTH,CELL_HEIGHT);
+	};
+}
+// ctx.fillRect(107,105,200,200);
+
+
+
+
+// 方法定义：让每个细胞获得自己的邻居细胞
+function setNeighbors(cells,canvas) {
+	var util=new UtilOfCells(cells,canvas);
+	util.setNeighborsPerCell();
+}
+
+
 // 细胞的对象表示--类
 function Cell (zuobiao) {
 	this.oldstate=0;
@@ -25,27 +53,7 @@ function Cell (zuobiao) {
 	this.zuobiao={x:zuobiao.x,y:zuobiao.y}
 	this.width=CELL_WIDTH;
 	this.height=CELL_WIDTH;
-	// this.draw=function (ctx) {
-	// 	// 将当前的状态赋值给旧状态，以给后续的细胞判断生命显示状态
-	// 	// var state_pd=this.state;
-	// 	// this.oldstate=this.state;
-	// 	// 判断是否绘制前先观察周围细胞状态，赋值未来下一次的生命状态
-	// 	var num=getCellLiveNum(this.zuobiao);
-	// 	this.futurestate=getFutureState(num);
-	// 	console.log('----this.futurestate:-------',this.futurestate);
-	// 	// 从全局状态表中得到之前的生存状态，判断是否绘制。
-	// 	// 如果状态为0,表示已经死亡，不绘制
-	// 	if (!this.state) {
-	// 		ctx.fillStyle='white';
-	// 		ctx.fillRect(this.zuobiao.x,this.zuobiao.y,this.width,this.height);
-	// 	}else{
-	// 		var color=this.color();
-	// 		ctx.fillStyle=color;
-	// 		ctx.fillRect(this.zuobiao.x,this.zuobiao.y,this.width,this.height);			
-	// 	}
-	// };
-	// 随机颜色，rgb值
-	
+	this.neighborsOfCells=[];
 
 	// 获取细胞生存环境活的细胞数字
 	this.getCellLiveNum=function () {
@@ -67,7 +75,7 @@ function Cell (zuobiao) {
 		return num;
 	}
 
-	// 获取未来的状态
+	// 刷新未来的状态
 	this.refreshFutureState=function () {
 		var num=this.getCellLiveNum();
 		// 规则
@@ -91,7 +99,7 @@ function Cell (zuobiao) {
 
 }
 
-
+// 产生细胞对象
 function MakeCells (canvas) {
 	// 获得细胞坐标数组
 	this.getZuoBiaos=function () {
@@ -114,116 +122,128 @@ function MakeCells (canvas) {
 			var x=Cell(zuobiaos[i]);
 			cells.push(new Cell(zuobiaos[i]));
 		};
+		// 
 		return cells
 	}
-	
+
 }
 
 
+// ------------测试MakeCells--------------------
 
-var cells =getCells();
-// for (var i = 0; i < cells.length; i++) {
-// 	console.log(cells[i].zuobiao.x,cells[i].zuobiao.y);
-// };
-// console.log(cells[38]);
-// 画出图形
-function drawState (cells) {
+function test_MakeCells(canvas) {
+	// console.log('hhaa');
+	var cells=new MakeCells(canvas).getCells();
+	console.log(cells);
 	for (var i = 0; i < cells.length; i++) {
-		cells[i].draw(ctx);
+		zb=cells[i].zuobiao;
+		console.log(zb);
 	};
-	// 循环一遍后，更新futurestate
+}
+
+// test_MakeCells(canvas);
+
+// ---------------------------------------------
+
+// 细胞工具，如：获取相邻细胞坐标。
+function UtilOfCells (cells,canvas) {
+	this.cells=cells;
+	this.canvas=canvas;
+
+	// 全局遍历cells,为每个cell赋值邻居细胞
+	this.setNeighborsPerCell=function () {
+		var cells=this.cells;
+		for (var i = 0; i < cells.length; i++) {
+			cells[i].neighborsOfCells.push(this.getNeighborsCells(cells[i]));
+		};
+	}
+
+	// 让细胞获得相邻细胞的坐标数据
+	this.getNeighborsCells=function (cell) {
+		// console.log(cell.zuobiao);
+		var newcells=[];
+		var nzbs=this.getNeighborsZuoBiao(cell.zuobiao);
+		// console.log('nzbs:',nzbs);
+
+		for (var i = 0; i < nzbs.length; i++) {
+			// console.log('nzbs[i].zuobiao:',nzbs[i]);
+			newcells.push(this.getCellFromZuobiao(nzbs[i],this.cells));
+		};
+		return newcells;
+	}
+
+	// 根据坐标，找到相应的细胞
+	this.getCellFromZuobiao=function (zuobiao,cells) {
+		// console.log(zuobiao);
+		for (var i = 0; i < cells.length; i++) {
+			if (cells[i].zuobiao.x==zuobiao.x &&cells[i].zuobiao.y==zuobiao.y ) {
+				return cells[i];
+			};
+		};
+		console.loe("未找到该坐标相对应的细胞:",zuobiao);
+		return "未找到该坐标相对应的细胞";
+	}
+	// 由坐标获得相邻坐标
+	this.getNeighborsZuoBiao=function (zuobiao) {
+		var neighbors=[];
+		var x=zuobiao.x;
+		var y=zuobiao.y;
+		// 一共8个
+		// [x-20,x,x+20],[y-20,y,y+20]
+		var xs=[x-CELL_WIDTH,x,x+CELL_WIDTH];
+		var ys=[y-CELL_WIDTH,y,y+CELL_WIDTH];
+		for (var i = 0; i < xs.length; i++) {
+			for (var j = 0; j < ys.length; j++) {
+				neighbors.push({x:xs[i],y:ys[j]});
+			};
+		};
+		// console.log(neighbors);	
+
+		// 在neighbors数组中去掉自己{x:x,y:y}。
+		var n_neighbors=[];
+		for (var i = 0; i < neighbors.length; i++) {
+			if (neighbors[i].x==zuobiao.x && neighbors[i].y==zuobiao.y ) {
+				continue;
+			};
+			n_neighbors.push(neighbors[i]);
+		};
+		neighbors=n_neighbors;
+		clearN=[]
+		// 去掉x,y<0 >canvas的坐标
+		for (var i = 0; i < neighbors.length; i++) {
+			if (neighbors[i].x<0||neighbors[i].x>=this.canvas.width||neighbors[i].y>=this.canvas.height||neighbors[i].y<0) {
+				continue;
+			};
+			clearN.push(neighbors[i]);
+		};
+
+		return clearN; 
+	}
+}
+
+// -------------------------测试UtilOfCells------------------------------------
+function test_UtilOfCells (cells,canvas) {
+
+	var util=new UtilOfCells(cells,canvas);
+	util.setNeighborsPerCell();
 	for (var i = 0; i < cells.length; i++) {
-		cells[i].state=cells[i].futurestate;
+		console.log(cells[i]);
 	};
 }
 
-// 根据坐标，找到相应的细胞
-function getCellFromCells (zuobiao) {
-	// 此处为闭包，可以直接访问cells,避免传递变量到Cell类中
-	for (var i = 0; i < cells.length; i++) {
-		if (cells[i].zuobiao.x==zuobiao.x &&cells[i].zuobiao.y==zuobiao.y ) {
-			return cells[i];
-		};
-	};
-
-}
-
-// var g=getCellFromCells({x:0,y:0});
-// alert(g);
-
-// 画出细胞的边框
-function drawBorders (cells) {
-	for (var i = 0; i < cells.length; i++) {
-		ctx.fillStyle='red';
-		ctx.strokeRect(cells[i].zuobiao.x,cells[i].zuobiao.y,cells[i].width,cells[i].height);
-		ctx.stroke();
-	};
-}
-drawBorders(cells);
-// 从一个坐标{x:0,y:0}得到邻居坐标
-function getNeighborsZuoBiao (zuobiao) {
-	var neighbors=[];
-	x=zuobiao.x;
-	y=zuobiao.y;
-	// 一共8个
-	// [x-20,x,x+20],[y-20,y,y+20]
-	var xs=[x-CELL_WIDTH,x,x+CELL_WIDTH];
-	var ys=[y-CELL_WIDTH,y,y+CELL_WIDTH];
-	for (var i = 0; i < xs.length; i++) {
-		for (var j = 0; j < ys.length; j++) {
-			neighbors.push({x:xs[i],y:ys[j]});
-		};
-	};
-	// console.log(neighbors);	
-
-	// 在neighbors数组中去掉自己{x:x,y:y}。
-	var n_neighbors=[];
-	for (var i = 0; i < neighbors.length; i++) {
-		if (neighbors[i].x==zuobiao.x && neighbors[i].y==zuobiao.y ) {
-			continue;
-		};
-		n_neighbors.push(neighbors[i]);
-	};
-	neighbors=n_neighbors;
-	clearN=[]
-	// 去掉x,y<0 >600的坐标
-	for (var i = 0; i < neighbors.length; i++) {
-		if (neighbors[i].x<0||neighbors[i].x>=canvas.width||neighbors[i].y>=canvas.height||neighbors[i].y<0) {
-			continue;
-		};
-		clearN.push(neighbors[i]);
-	};
-
-	return clearN; 
-}
-
-
-// 输入坐标数组，得到数组对应的细胞数组；
-function getNeighborsCells(NeighborZuoBiaos) {
-	var cells=[];
-	for (var i = 0; i < NeighborZuoBiaos.length; i++) {
-		var cell=getCellFromCells(NeighborZuoBiaos[i]);
-		cells.push(cell);
-	};
-	return cells;
-}
+test_UtilOfCells(cells,canvas);
 
 
 
 
-// ------------------------测试------------------------------------
-function test_getNeighborsCells() {
-	// x: 0, y: 580
-	var res=getNeighborsZuoBiao({x:0,y:580});
-	console.log(res);
-	var res_cells=getNeighborsCells(res);
-	console.log(res_cells);
-	for (var i = 0; i < res_cells.length; i++) {
-		console.log(res_cells[i].state);
-	};
-}
 
-// test_getNeighborsCells();
+
+
+
+
+
+
+
 
 // ---------------------------工具-----------------------
 // 0-n的随机数
